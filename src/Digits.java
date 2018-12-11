325e912d19244bceb5fd0b8b567eac8ef05afab7
BT_1N3_06@bktech.local <BT_1N3_06@bktech.local>
2018-12-11 09:19:55 -0500

    Initial commit

---
+++ src/Digits.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Digits {
+
+    private int num;
+    private ArrayList<Integer> digits;
+
+    public Digits(int num)
+    {
+        digits = new ArrayList<Integer>();
+        String numstr = String.valueOf(num);
+        for(int i = 0; i < numstr.length(); i++)
+        {
+            int j = Character.digit(numstr.charAt(i),10);
+            Integer k = new Integer(j);
+            digits.add(k);
+        }
+    }
+
+    public boolean isStrictlyIncreasing()
+    {
+        for(int i = 0; i < digits.size() - 1; i++)
+        {
+            if(digits.get(i) < digits.get(i + 1))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String getDigitList()
+    {
+        System.out.println(Arrays.deepToString(digits.toArray()));
+        return "";
+    }
+}
